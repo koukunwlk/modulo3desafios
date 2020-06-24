c6a795ba95947aede704f5658f3caab1e7bd287e
Moacir Amaro <moacirjgt@gmail.com>
2020-06-23 21:17:56 -0300

    Adicionado a pagina courses

--- server.js
+++ server.js
@@ -19,6 +19,23 @@ server.get('/contents', (req, res)=>{
     res.render('contents', {contents})
 })
 
+server.get('/courses/:id', (req, res)=>{
+    const id = req.params.id
+
+    const content = contents.find((content)=>{
+        if(content.id == id){
+            return true
+        }
+    })
+
+    if(!content){
+        return res.send("Content not found")
+    }
+    
+    return res.render('courses', {content})
+    
+})
+
 server.use((req, res)=>{
     res.status(404).render('not-found')
 })
